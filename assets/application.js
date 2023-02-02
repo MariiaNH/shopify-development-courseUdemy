a2860b925d6591132cc972cec4591406c64ac8f8
Mariia Nedilko <mariia.nedilko@gmail.com>
2023-02-02 19:20:45 +0100

    main course

---
+++ assets/application.js
@@ -0,0 +1,10 @@
+
+if (document.getElementById('sort_by') !== null) {
+    document.querySelector('#sort_by').addEventListener('change', function (e) {
+        const url = new URL(window.location.href);
+
+        url.searchParams.set("sort_by", e.currentTarget.value);
+
+        window.location = url.href;
+    });
+}
